275bdedd4cdbd4467718284772072676d8257300
SUBHADEEP BISWAS <147643078+subhadeep104647@users.noreply.github.com>
2026-03-05 00:08:12 +0530

    update loging.html, home.html, app.py

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/login")
+@app.route("/")
 def login():
     return render_template("login.html")
+
+@app.route("/home", methods=["POST"])
+def Home():
+    number= request.form.get("number")
+    
+    if number == "123456":
+        return render_template("home.html")
+    
+    else:
+        return "Invalid Number"
+    
 
 
 
