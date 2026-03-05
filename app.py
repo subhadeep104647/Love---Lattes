49e6cafe5dfc8526622d99a184cefcccd0789713
SUBHADEEP BISWAS <147643078+subhadeep104647@users.noreply.github.com>
2026-03-05 21:54:11 +0530

    create menu

--- app.py
+++ app.py
@@ -6,18 +6,33 @@ app = Flask(__name__)
 def login():
     return render_template("login.html")
 
-@app.route("/home", methods=["POST"])
-def Home():
-    number= request.form.get("number")
-    
-    if number == "123456":
-        return render_template("home.html")
-    
+
+@app.route("/home", methods=["GET","POST"])
+def home():
+    if request.method == "POST":
+        number = request.form.get("number")
+
+        if number == "123456":
+            return render_template("home.html")
+        else:
+            return "Invalid Number"
     else:
-        return "Invalid Number"
-    
+        return render_template("home.html")
 
 
+@app.route("/menu")
+def menu():
+    return render_template("menu.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
 
 
 if __name__ == "__main__":
