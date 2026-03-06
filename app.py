8af7fdf491624d06d202d93e77ee88350f45541e
SUBHADEEP BISWAS <147643078+subhadeep104647@users.noreply.github.com>
2026-03-06 14:37:24 +0530

    Updated memu.html, menu.css, app.py and add cart.html, cart.css

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
+app.secret_key = "love_lattes_secret"
+
 
 @app.route("/")
 def login():
@@ -22,7 +24,58 @@ def home():
 
 @app.route("/menu")
 def menu():
-    return render_template("menu.html")
+    cart_count = len(session.get("cart", []))
+    return render_template("menu.html", cart_count=cart_count)
+
+
+# ADD ITEM TO CART
+@app.route("/add_to_cart", methods=["POST"])
+def add_to_cart():
+
+    item = request.form.get("item")
+    price = int(request.form.get("price"))
+
+    if "cart" not in session:
+        session["cart"] = []
+
+    session["cart"].append({
+        "item": item,
+        "price": price
+    })
+
+    session.modified = True
+
+    return redirect(url_for("menu"))
+
+
+# VIEW CART
+@app.route("/cart")
+def cart():
+
+    cart_items = session.get("cart", [])
+
+    total = sum(item["price"] for item in cart_items)
+
+    return render_template("cart.html", cart=cart_items, total=total)
+
+
+# REMOVE ITEM
+@app.route("/remove/<int:index>")
+def remove(index):
+
+    session["cart"].pop(index)
+    session.modified = True
+
+    return redirect(url_for("cart"))
+
+
+# PLACE ORDER
+@app.route("/order")
+def order():
+
+    session.pop("cart", None)
+
+    return "🎉 Order placed successfully! Your Order is on the way, Thank you "
 
 
 @app.route("/about")
